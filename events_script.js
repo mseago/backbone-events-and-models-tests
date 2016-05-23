b371e166e2ebb7cc3ad457b53a490ae3e7df0df8
Melissa Seago <melissa.seago@gmail.com>
2016-05-23 15:17:11 -0500

    answered 1-5

--- events_script.js
+++ events_script.js
@@ -5,7 +5,27 @@ function Animal(type) {
 }
 
 function Circus() {
+  this.animals =[];
+}
+_.extend(Circus.prototype, Backbone.Events);
+
+Circus.prototype.add = function(animal) {
+  this.animals.push(animal);
+}
+
+Circus.prototype.animalCount = function() {
+  return this.animals.length;
 }
 
 function Ringmaster() {
+  this.isCalm = true;
+}
+
+Ringmaster.prototype.watch = function(watchingShow) {
+  var self = this;
+  watchingShow.on ("circus", function(){
+    if (Animal === "lion") {
+      self.isCalm === false;
+    }
+  });
 }
